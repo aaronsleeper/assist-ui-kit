ec322d55edff5d2769516282a283cdff7917bed1
Aaron Sleeper <asleeper@fairwaytech.com>
2018-01-10 12:20:40 -0800

    link list group radio toggle

---
+++ app/js/scripts.js
@@ -0,0 +1,30 @@
+/* -- -- -- -- -- -- -- -- -- -- 
+Lottie Animations
+-- -- -- -- -- -- -- -- -- -- */
+
+// Loader
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('loader'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animations/loader.json'
+})
+
+
+
+/* -- -- -- -- -- -- -- -- -- -- 
+Input Interactions
+-- -- -- -- -- -- -- -- -- -- */
+
+// Link List Group
+// Radio Icon Toggle
+$('.link-list-group li').click(function() {
+    // if this isn't the selected list item
+    if(!$(this).hasClass('selected')) {
+        // remove .selected from the currently selected list item
+        $('.link-list-group li.selected').removeClass('selected');
+    }
+    // toggle .selected on the clicked list item
+    $(this).toggleClass('selected');
+});
