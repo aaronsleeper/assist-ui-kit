e57c6cfcd058bb910dc8e46645a6b62b5b1f3dc9
Aaron Sleeper <asleeper@fairwaytech.com>
2018-01-30 12:47:22 -0800

    convert loader from id to class

--- app/js/app.js
+++ app/js/app.js
@@ -16,13 +16,17 @@ Loader Animation
 -- -- -- -- -- -- -- -- -- -- */
 
 // Init
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('loader'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'animations/loader.json'
-})
+var loader = document.getElementsByClassName('loader')
+
+for (var i = 0; i < loader.length; i++) {
+    var animation = bodymovin.loadAnimation({
+        container: loader[i],
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/loader.json'
+    })
+}
 // Focus the select institution input field
 // $('#discipline-search').focus();
 
--- app/scripts/loader.js
+++ app/scripts/loader.js
@@ -3,10 +3,14 @@ Loader Animation
 -- -- -- -- -- -- -- -- -- -- */
 
 // Init
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('loader'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'animations/loader.json'
-})
+var loader = document.getElementsByClassName('loader')
+
+for (var i = 0; i < loader.length; i++) {
+    var animation = bodymovin.loadAnimation({
+        container: loader[i],
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/loader.json'
+    })
+}
